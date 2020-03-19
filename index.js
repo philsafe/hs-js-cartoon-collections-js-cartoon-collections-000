306e1109396c90381e30da789f7162b96d92592c
Philip Phipps <phippsphilip@yahoo.com>
2020-03-19 21:20:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function dwarfRollCall(dwarves) {
- 
+ var endString = ""
+ for (var i=1; i<dwarves.length; i++)
+ { endString += (i).toString()+". "+ dwarves[i] +"";}; 
+ return endString;
 }
 
 function summonCaptainPlanet(planeteerCalls){
